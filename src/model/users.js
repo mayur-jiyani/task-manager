335105e1af67e780acb3889438b1bee3e403e3f3
mayur-jiyani <mayur.jiyani@marutitech.com>
2022-01-18 16:02:25 +0530

    API authentication sec completed

--- src/model/users.js
+++ src/model/users.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const validator = require("validator")
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./tasks');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -112,6 +113,14 @@ userSchema.pre('save', async function (next) {
     }
     next()
 })
+
+userSchema.pre('remove', async function (next) {
+    const user = this
+
+    await Task.deleteMany({ owner: user._id })
+    next()
+})
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
